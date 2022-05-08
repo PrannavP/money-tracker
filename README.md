6258cf9f112df31692b6950449e9a6400b924f22
PrannavP <ramsarilekhi@gmail.com>
2022-05-08 20:12:48 +0545

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Just a simple money tracker which tracks your income and expense (with graphs soon).
+Just a simple money tracker which tracks your income and expense.
--- scripts/home.js
+++ scripts/home.js
@@ -1,46 +1,50 @@
-const userName = document.getElementById('username');
-const userAmount = document.getElementById('useramount');
-const userCurrency = document.getElementById('usercurrency');
+// const userName = document.getElementById('username');
+// const userAmount = document.getElementById('useramount');
+// const userCurrency = document.getElementById('usercurrency');
 
-// fetching data from localstorage
-function getData(){
-    var storedName = localStorage.getItem('name');
-    var storedAmount = localStorage.getItem('amount');
-    var storedCurrency = localStorage.getItem('currency');
+// // fetching data from localstorage
+// function getData(){
+//     var storedName = localStorage.getItem('name');
+//     var storedAmount = localStorage.getItem('amount');
+//     var storedCurrency = localStorage.getItem('currency');
 
-    console.log(storedName, storedAmount, storedCurrency);
+//     console.log(storedName, storedAmount, storedCurrency);
 
-    // userName.innerText = storedName;
-    // userAmount.innerText = storedAmount;
-    // userCurrency.innerText = storedCurrency;
+//     // userName.innerText = storedName;
+//     // userAmount.innerText = storedAmount;
+//     // userCurrency.innerText = storedCurrency;
     
-    alert(`Welcome ${storedName} dumbass. Your balance is ${storedAmount} poor kid.`);
+//     // alert(`Welcome ${storedName} dumbass. Your balance is ${storedAmount} poor kid.`);
+// };
+
+// getData();
+
+// // check if localstorage does not exists
+// function checkLocalStorage(){
+//     if(localStorage.getItem('name') === null && localStorage.getItem('amount') === null && localStorage.getItem('currency') === null){
+//         window.location.href = `index.htm`;
+//     };
+// };
+
+// // navigation system
+// const dashboard = document.getElementById('dashboard');
+// const report = document.getElementById('report');
+// const settings = document.getElementById('settings');
+
+// // dashboard onclick
+// // dashboard.addEventListener('click', () => {
+// //     window.location.href = '../home/home.htm';
+// // });
+
+// function test(){
+//     window.location.href = '../report/report.htm';
+// };
+
+// report.addEventListener('click', test);
+
+function getTime(){
+    var a = new Date();
+    console.log(`Current time is ${a.getHours()}:${a.getMinutes()}:${a.getSeconds()}`);
 };
 
-getData();
-
-// check if localstorage does not exists
-function checkLocalStorage(){
-    if(localStorage.getItem('name') === null && localStorage.getItem('amount') === null && localStorage.getItem('currency') === null){
-        window.location.href = `index.htm`;
-    };
-};
-
-// navigation system
-const dashboard = document.getElementById('dashboard');
-const report = document.getElementById('report');
-const settings = document.getElementById('settings');
-
-// dashboard onclick
-// dashboard.addEventListener('click', () => {
-//     window.location.href = '../home/home.htm';
-// });
-
-function test(){
-    window.location.href = '../report/report.htm';
-    // alert('report clicked');
-};  
-
-// report onclick
-
-report.addEventListener('click', test);
+// setInterval(getTime, '1000');
